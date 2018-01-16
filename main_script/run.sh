0a131c75e4ef022f3eb0a8a6165f5c07cbde987a
ayush493fk <ayush.bhatnagar@flipkart.com>
2018-01-16 16:34:44 +0530

    small typo

--- main_script/run.sh
+++ main_script/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$ENV" == "neo-ctr"]; then
+if [ "$ENV" == "neo-ctr" ]; then
     python /home/ayush.bhatnagar/main_script/hello.py >> /home/ayush.bhatnagar/neo_CTR
-elif [ "$ENV" == "neo-cvr"]; then
+elif [ "$ENV" == "neo-cvr" ]; then
     python /home/ayush.bhatnagar/main_script/hello.py >> /home/ayush.bhatnagar/neo_CVR
 else
     python /home/ayush.bhatnagar/main_script/hello.py >> /home/ayush.bhatnagar/something
