4ceb29f1cb4db9a1ea9e5c2a4eae9dbb6f3796fc
ayush493fk <ayush.bhatnagar@flipkart.com>
2018-01-16 16:31:31 +0530

    Differentiate between ctr and cvr

--- main_script/run.sh
+++ main_script/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-python /home/ayush.bhatnagar/main_script/hello.py >> /home/ayush.bhatnagar/dummy_file
+if [ "$ENV" == "neo-ctr"]; then
+    python /home/ayush.bhatnagar/main_script/hello.py >> /home/ayush.bhatnagar/neo_CTR
+elif [ "$ENV" == "neo-cvr"]; then
+    python /home/ayush.bhatnagar/main_script/hello.py >> /home/ayush.bhatnagar/neo_CVR
+else
+    python /home/ayush.bhatnagar/main_script/hello.py >> /home/ayush.bhatnagar/something
+fi
+
